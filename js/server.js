7d0aba2017758a9e3c451ffd54afa347a4436146
Toni <50109298+scarpa27@users.noreply.github.com>
2022-10-31 16:56:57 +0100

    cors

--- js/server.js
+++ js/server.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const app = express();
 
-const app_name = "SessionRecorderUI"
+const app_name = "SessionRecorderUI";
+const whitelist = ['https://session-recorder.herokuapp.com/', 'https://session-recorder-backend.herokuapp.com/']
 
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) {
+            return callback(null, true);
+        }
+        if (whitelist.indexOf(origin) === -1) {
+            let msg = "Forbidden access to "+origin+" due to cors policy";
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}
+
+app.use(cors(corsOptions));
 app.use(express.static(path.join(__dirname, '../dist/'+app_name)));
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
