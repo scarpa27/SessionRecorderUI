89a56270fea3dd90f244339b568439c47b7c7b09
Toni <50109298+scarpa27@users.noreply.github.com>
2022-10-31 16:34:48 +0100

    server.js

--- js/server.js
+++ js/server.js
@@ -4,7 +4,13 @@ const app = express();
 
 const app_name = "SessionRecorderUI"
 
-app.use(express.static(__dirname + '../dist/'+app_name));
-app.get('/*', (req, res) => res.sendFile('index.html', {root: 'dist/'+app_name+'/'}))
+app.use(express.static(path.join(__dirname, '../dist/'+app_name)));
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, \ Authorization');
+    next();
+});
+app.get('*', (req, res) => res.sendFile('index.html', {root: 'dist/'+app_name+'/'}));
 
 app.listen(process.env.PORT || 8080);
