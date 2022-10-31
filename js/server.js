e1748401c80ef51dee32b6731d35d715bc6e2127
Toni <50109298+scarpa27@users.noreply.github.com>
2022-10-31 16:17:04 +0100

    server.js

--- js/server.js
+++ js/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+const app_name = "SessionRecorderUI"
+
+app.use(express.static(__dirname + '/dist/'+app_name));
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/'+app_name+'/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
